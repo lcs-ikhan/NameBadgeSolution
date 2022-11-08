3fcd200446192ed9d8dc34596b59c4947f62676d
Isaad Khan <IKhan@lcs.on.ca>
2022-11-08 14:18:35 -0500

    Initial Commit

---
+++ NameBadgeSolution/NameBadgeSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameBadgeSolutionApp.swift
+//  NameBadgeSolution
+//
+//  Created by Isaad Khan on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct NameBadgeSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
